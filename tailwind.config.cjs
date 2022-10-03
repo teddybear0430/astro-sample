d719718171ad1df4ac3340120d535f3024782632
teddybear0430 <teddybear0430@hotmail.com>
2022-10-04 00:27:53 +0900

    feat: Added tailwind.config.js

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.astro', './src/**/*.tsx'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
